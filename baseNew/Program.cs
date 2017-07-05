173ca943574f111854a951f460cd244b819b8b2a
martianrobotics <jupiter.arias@gmail.com>
2017-07-05 14:22:50 -0400

    Add project files.

---
+++ baseNew/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baseNew
+{
+    class xBase
+    {
+        //public void Execute()
+        public virtual void Execute()
+        {
+            Console.WriteLine("Base.Execute");
+        }
+    }
+
+    class xDerived:xBase
+    {
+        public override void Execute()
+        {
+            Console.WriteLine("Derived.Execute");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            xBase b = new xBase();
+            b.Execute();
+            b = new xDerived();
+            b.Execute();
+            Console.ReadKey();
+        }
+    }
+}
